5953b8630f71df64586e534d4333ca69a71d8824
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-10-17 12:30:50 +0600

    dark mode logo added and theme toglgle issue fixed

--- components/(Global)/HomeNav/NavBar/NavBar.jsx
+++ components/(Global)/HomeNav/NavBar/NavBar.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Image from "next/image";
 import logo from '../../../../assets/logo/logo-go.png'
+import darklogo from '../../../../assets/logo/n-dark.png'
 import NavItem from "../NavItem/NavItem";
 const NavBar = () => {
   return (
-    <nav className="py-6 container mx-auto flex items-center justify-between dark:bg-primary">
-      <div className="logo ">
+    <nav className="py-6 container mx-auto flex items-center justify-between ">
+      <div className="logo dark:hidden">
         <Image
           src={logo}
+          width={300}
+          height={500}
+          alt="Picture of the site logo"
+        ></Image>
+      </div> 
+      {/* dark logo */}
+      <div className="logo hidden dark:block">
+        <Image
+          src={darklogo}
           width={300}
           height={500}
           alt="Picture of the site logo"
