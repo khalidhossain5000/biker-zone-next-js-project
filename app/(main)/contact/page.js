8c984dbae360b423fca359f5ba1b8422e9331808
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-11-01 10:50:00 +0600

    nodemailer added

--- app/(main)/contact/page.js
+++ app/(main)/contact/page.js
@@ -1,15 +1,17 @@
 "use client";
 import Loading from "@/app/loading";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Page = () => {
   const [loading, setLoading] = useState(true);
+const divRef=useRef(null)
 
+console.log('this is div ref in contact',divRef);
   useEffect(() => {
     // Show loader for at least 5 seconds
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 50000);
+    }, 5000);
 
     // Cleanup timer on unmount
     return () => clearTimeout(timer);
@@ -20,7 +22,7 @@ const Page = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-[#0f1115] text-white">
+    <div ref={divRef} className="flex flex-col items-center justify-center h-screen bg-[#0f1115] text-white">
       <h2 className="text-3xl font-bold mb-3">Contact Us</h2>
       <p className="text-gray-400 text-lg">We’d love to hear from you!</p>
     </div>
